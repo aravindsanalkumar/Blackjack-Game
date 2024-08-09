4c7c597ecc97328cdfa382daf09fb36dde62fe3f
aravind <aravindsanalkumar@gmail.com>
2024-08-09 14:20:09 +0530

    function created

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-let firstCard = 4;
-let secondCard = 3;
+let firstCard = 14;
+let secondCard = 7;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
+let message = "";
 
-console.log(sum);
+let message_el = document.getElementById("message_el");
+let sum_el = document.getElementById("sum_el");
 
-if(sum <= 20){
-    console.log("Do you want to draw a new card? 🙂")
+
+function startGame(){
+    if(sum <= 20){
+    message = "Do you want to draw a new card? 🙂";
+    }
+    else if(sum === 21){
+        hasBlackJack = true;
+        message = "Wohoo! You've got Blackjack! 🥳";
+    }
+    else{
+        isAlive = false;
+        message = "You're out of the game! 😭";
+    }
+    message_el.textContent = message;
 }
-else if(sum === 21){
-    hasBlackJack = true;
-    console.log("Wohoo! You've got Blackjack! 🥳");
-}
-else{
-    isAlive = false;
-    console.log("You're out of the game! 😭")
-}
+
+
