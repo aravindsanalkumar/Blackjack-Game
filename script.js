0b5811cc154dcd885363fc8c58291118a373e331
aravind <aravindsanalkumar@gmail.com>
2024-08-09 14:02:27 +0530

    flag variables added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let firstCard = 4;
 let secondCard = 3;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
 
 console.log(sum);
 
@@ -8,8 +10,10 @@ if(sum <= 20){
     console.log("Do you want to draw a new card? 🙂")
 }
 else if(sum === 21){
+    hasBlackJack = true;
     console.log("Wohoo! You've got Blackjack! 🥳");
 }
 else{
+    isAlive = false;
     console.log("You're out of the game! 😭")
 }
